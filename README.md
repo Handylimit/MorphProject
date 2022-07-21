ed2a01eba77eca7361857f12f96e68b7a10cd6a3
Handylimit <79088275+Handylimit@users.noreply.github.com>
2022-07-21 21:12:30 +0100

    First Submit
    Basic empty set-up

---
+++ README.md
@@ -0,0 +1,3 @@
+# MorphProject
+
+Developed with Unreal Engine 4
---
+++ Source/MorphProject/MorphProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MorphProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MORPHPROJECT_API AMorphProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
